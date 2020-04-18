a1684afb81bc79c1198831a1f98856702d7962f8
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-18 12:07:10 -0600

    database is ready for player seeds

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -8,7 +8,7 @@ module.exports = {
                 .create(req.body)
                 .then(dbModel => res.json(dbModel))
                 .catch(err => res.status(422).json(err));
-          },
+          }
 
 //   findAll: function(req, res) {
 //     db.Book
---
+++ scripts/seedDB.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/tradeandplay"
+);
+
+const playerSeed = [
+    {
+        id: 8447400,
+        fullName: "Wayne Gretsky",
+        profileLink: "/api/v1/people/8447400/",
+        season: 19841985
+    }
+]
+
+db.PlayerStats.remove({})
+  .then(() => db.PlayerStats.collection.insertMany(playerSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
