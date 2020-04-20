8df70f2d4e678f1cd0907620f1437c13c91f11fa
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-20 16:09:41 -0600

    lint replaced double quotes with single quotes

--- models/players.js
+++ models/players.js
@@ -1,109 +1,109 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const playerSchema = new Schema({
-    id: {
-        type: Number,
-        required: "Must have a player ID"
-    },
-    fullName: {
-        type: String,
-        trim: true,
-        required: "Enter a name"
-    },
-    profileLink: {
-        type: String,
-        required: "Must have profile link"
-    },
-    season: {
-        type: Number,
-        required: "Must have a season"
-    },
-    // Stats start here
-    timeOnIce: {
-        type: String
-    },
-    assists: {
-        type: Number
-    },
-    goals: {
-        type: Number
-    },
-    pim: {
-        type: Number
-    },
-    shots: {
-        type: Number
-    },
-    games: {
-        type: Number
-    },
-    hits: {
-        type: Number
-    },
-    powerPlayGoals: {
-        type: Number
-    },
-    powerPlayPoints: {
-        type: Number
-    },
-    powerPlayTimeOnIce: {
-        type: String
-    },
-    evenTimeOnIce: {
-        type: String
-    },
-    penaltyMinutes: {
-        type: String
-    },
-    faceOffPct: {
-        type: Number
-    },
-    shotPct: {
-        type: Number
-    },
-    gameWinningGoals: {
-        type: Number
-    },
-    overTimeGoals: {
-        type: Number
-    },
-    shortHandedGoals: {
-        type: Number
-    },
-    shortHandedPoints: {
-        type: Number
-    },
-    shortHandedTimeOnIce: {
-        type: String
-    },
-    blocked: {
-        type: Number
-    },
-    plusMinus: {
-        type: Number
-    },
-    points: {
-        type: Number
-    },
-    shifts: {
-        type: Number
-    },
-    timeOnIcePerGame: {
-        type: String
-    },
-    evenTimeOnIcePerGame: {
-        type: String
-    },
-    shortHandedTimeOnIcePerGame: {
-        type: String
-    },
-    powerPlayTimeOnIcePerGame: {
-        type: String
-    },
-});
+  id: {
+    type: Number,
+    required: 'Must have a player ID'
+  },
+  fullName: {
+    type: String,
+    trim: true,
+    required: 'Enter a name'
+  },
+  profileLink: {
+    type: String,
+    required: 'Must have profile link'
+  },
+  season: {
+    type: Number,
+    required: 'Must have a season'
+  },
+  // Stats start here
+  timeOnIce: {
+    type: String
+  },
+  assists: {
+    type: Number
+  },
+  goals: {
+    type: Number
+  },
+  pim: {
+    type: Number
+  },
+  shots: {
+    type: Number
+  },
+  games: {
+    type: Number
+  },
+  hits: {
+    type: Number
+  },
+  powerPlayGoals: {
+    type: Number
+  },
+  powerPlayPoints: {
+    type: Number
+  },
+  powerPlayTimeOnIce: {
+    type: String
+  },
+  evenTimeOnIce: {
+    type: String
+  },
+  penaltyMinutes: {
+    type: String
+  },
+  faceOffPct: {
+    type: Number
+  },
+  shotPct: {
+    type: Number
+  },
+  gameWinningGoals: {
+    type: Number
+  },
+  overTimeGoals: {
+    type: Number
+  },
+  shortHandedGoals: {
+    type: Number
+  },
+  shortHandedPoints: {
+    type: Number
+  },
+  shortHandedTimeOnIce: {
+    type: String
+  },
+  blocked: {
+    type: Number
+  },
+  plusMinus: {
+    type: Number
+  },
+  points: {
+    type: Number
+  },
+  shifts: {
+    type: Number
+  },
+  timeOnIcePerGame: {
+    type: String
+  },
+  evenTimeOnIcePerGame: {
+    type: String
+  },
+  shortHandedTimeOnIcePerGame: {
+    type: String
+  },
+  powerPlayTimeOnIcePerGame: {
+    type: String
+  }
+})
 
-const PlayerStats = mongoose.model("PlayerStats", playerSchema);
+const PlayerStats = mongoose.model('PlayerStats', playerSchema)
 
-module.exports = PlayerStats;
+module.exports = PlayerStats
