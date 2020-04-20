78aebaee803b586345bde8df71490e6db1d83b08
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-20 16:22:32 -0600

    imported buttonappbar/navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import ButtonAppBar from './components/NavBar'
+// import Login from './components/login'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ButtonAppBar />
+      {/* <Login/> */}
     </div>
-  );
+  )
 }
 
-export default App;
-
-//test
+export default App
