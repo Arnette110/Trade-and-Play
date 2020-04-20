2e447ad9f68ebded8df6fe66cefbe8f0875c6f82
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-20 17:54:06 -0600

    Deleted example comments

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/tradeandplay"
 );
