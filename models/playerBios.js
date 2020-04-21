82759bc746c9aabdbf86ee7b11c00b46f98d0f4e
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-20 18:30:53 -0600

    Reformatted schemas

--- models/playerBios.js
+++ models/playerBios.js
@@ -1,77 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const playerBioSchema = new Schema({
-    id: {
-        type: Number,
-        required: "Must have a player ID"
-    },
-    fullName: {
-        type: String,
-        required: "Must have a player fullName"
-    },
-    link: {
-        type: String,
-    },
-    firstName: {
-        type: String,
-        required: "Must have a player fullName"
-    },
-    lastName: {
-        type: String,
-        required: "Must have a player fullName"
-    },
-    primaryNumber: {
-        type: String,
-    },
-    birthDate: {
-        type: String,
-    },
-    currentAge: {
-        type: Number,
-    },
-    birthCity: {
-        type: String,
-    },
-    birthCountry: {
-        type: String,
-    },
-    nationality: {
-        type: String,
-    },
-    height: {
-        type: String,
-    },
-    weight: {
-        type: Number,
-    },
-    active: {
-        type: Boolean,
-    },
-    alternateCaptain: {
-        type: Boolean,
-    },
-    captain: {
-        type: Boolean,
-    },
-    rookie: {
-        type: Boolean,
-    },
-    shootsCatches: {
-        type: String,
-    },
-    rosterStatus: {
-        type: String,
-    },
-    currentTeam: {
-        type: Object,
-    },
-    primaryPosition: {
-        type: Object,
-    },
-});
+  id: { type: Number, required: true },
+  fullName: { type: String, required: true },
+  link: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  primaryNumber: { type: String },
+  birthDate: { type: String },
+  currentAge: { type: Number },
+  birthCity: { type: String },
+  birthCountry: { type: String },
+  nationality: { type: String },
+  height: { type: String },
+  weight: { type: Number },
+  active: { type: Boolean },
+  alternateCaptain: { type: Boolean },
+  captain: { type: Boolean },
+  rookie: { type: Boolean },
+  shootsCatches: { type: String },
+  rosterStatus: { type: String },
+  currentTeam: { type: Object },
+  primaryPosition: { type: Object }
+})
 
-const PlayerBios = mongoose.model("PlayerBios", playerBioSchema);
+const PlayerBios = mongoose.model('PlayerBios', playerBioSchema)
 
-module.exports = PlayerBios;
+module.exports = PlayerBios
--- models/playerStats.js
+++ models/playerStats.js
@@ -3,18 +3,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const playerSchema = new Schema({
-  id: {
-    type: Number,
-    required: 'Must have a player ID'
-  },
-  season: {
-    type: Number,
-    required: 'Must have a season'
-  },
-  stat: {
-    type: Object,
-    required: 'Must have stats'
-  }
+  id: { type: Number, required: true },
+  season: { type: Number, required: true },
+  stat: { type: Object, required: true }
 })
 
 const PlayerStats = mongoose.model('PlayerStats', playerSchema)
