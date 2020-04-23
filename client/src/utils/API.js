f1428c3a41ce4bcd4467392ff064ae650855bf25
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-22 20:20:02 -0600

    backend post to DB

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+//   // Gets all books
+//   getBooks: function() {
+//     return axios.get("/api/books");
+//   },
+//   // Gets the book with the given id
+//   getBook: function(id) {
+//     return axios.get("/api/books/" + id);
+//   },
+//   // Deletes the book with the given id
+//   deleteBook: function(id) {
+//     return axios.delete("/api/books/" + id);
+//   },
+  // Saves a book to the database
+  savePlayer: function(playerData) {
+    return axios.post("/api/players", playerData);
+  }
+};
