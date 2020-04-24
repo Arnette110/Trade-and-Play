f317973aee7acf597acee4e407497975a7af1bb7
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-23 19:10:08 -0600

    Moved elements into Grid component

--- client/src/pages/PlayerCreate.js
+++ client/src/pages/PlayerCreate.js
@@ -1,43 +1,62 @@
 import React, { useState } from "react";
-import { Container, TextField, Button } from '@material-ui/core';
+import { Container, TextField, Button, Typography, Grid } from '@material-ui/core';
 import API from '../utils/API';
 
-function PlayerCreate(){
+function PlayerCreate() {
 
   const [form, setForm] = useState({
     playerId: '',
-    season: ''
+    season: '',
+    bioObj: {},
+    statsObj: {}
   })
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setForm({...form, [name]: value})
+    setForm({ ...form, [name]: value })
   };
+
+  function addIdAndSetForm(obj) {
+    let stats = obj
+    stats.id = form.playerId
+    console.log('stats: ', stats)
+    setForm({ ...form, statsObj: stats })
+  }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    if (form.playerId) {
+    if (form.playerId && form.season) {
+      // gets Bio information
       API.getBio(form.playerId)
-        .then(res => console.log(res.data.people[0]))
+        .then(res => setForm({ ...form, bioObj: res.data.people[0] }))
         .catch(err => console.log(err));
+      // gets player stats
       API.getStats(form.playerId, form.season)
-        .then(res => console.log(res.data.stats[0].splits[0]))
+        .then(res => addIdAndSetForm(res.data.stats[0].splits[0]))
         .catch(err => console.log(err));
-      }
+    }
   };
 
   return (
     <Container>
-      <div>
-        <form>
-          <TextField id="standard-basic" label="PlayerId" onChange={handleInputChange} name="playerId" />
-          <TextField id="standard-basic" label="Season"  onChange={handleInputChange} name="season" />
-          <Button variant="contained" onClick={handleFormSubmit}>Submit</Button>
-        </form>
-      </div>
-      <div>
-        return JSON here
-      </div>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <form>
+            <TextField id="standard-basic" label="PlayerId" onChange={handleInputChange} name="playerId" />
+            <TextField id="standard-basic" label="Season" onChange={handleInputChange} name="season" />
+            <Button variant="contained" onClick={handleFormSubmit}>Submit</Button>
+          </form>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h6'>Player Bio Data</Typography>
+          <pre>{JSON.stringify(form.bioObj, null, 2)}</pre>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h6'>Player Stats Data</Typography>
+          <pre>{JSON.stringify(form.statsObj, null, 2)}</pre>
+        </Grid>
+      </Grid>
+
     </Container>
   )
 }
