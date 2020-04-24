947c365cdb57ab3a19df066f4ee23296a4683dc4
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-23 20:20:02 -0600

    switched from axios to using fetch.

--- client/src/pages/PlayerCreate.js
+++ client/src/pages/PlayerCreate.js
@@ -23,19 +23,30 @@ function PlayerCreate() {
     setForm({ ...form, statsObj: stats })
   }
 
-  function handleFormSubmit(event) {
+  async function handleFormSubmit(event) {
     event.preventDefault();
     if (form.playerId && form.season) {
-      // gets Bio information
-      API.getBio(form.playerId)
-        .then(res => setForm({ ...form, bioObj: res.data.people[0] }))
-        .catch(err => console.log(err));
-      // gets player stats
-      API.getStats(form.playerId, form.season)
-        .then(res => addIdAndSetForm(res.data.stats[0].splits[0]))
-        .catch(err => console.log(err));
-    }
-  };
+      let bio = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${form.playerId}`)
+      let bioData = await bio.json()
+      let stats = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${form.playerId}/stats?stats=statsSingleSeason&season=${form.season}`)
+      let statsData = await stats.json()
+      statsData.stats[0].splits[0].id = form.playerId
+      setForm({...form, bioObj: bioData.people[0], statsObj: statsData.stats[0].splits[0]})
+    };
+  }
+  // function handleFormSubmit(event) {
+  //   event.preventDefault();
+  //   if (form.playerId && form.season) {
+  //     // gets Bio information
+  //     API.getBio(form.playerId)
+  //       .then(res => setForm({ ...form, bioObj: res.data.people[0] }))
+  //       .catch(err => console.log(err));
+  //     // gets player stats
+  //     API.getStats(form.playerId, form.season)
+  //       .then(res => addIdAndSetForm(res.data.stats[0].splits[0]))
+  //       .catch(err => console.log(err));
+  //   }
+  // };
 
   return (
     <Container>
