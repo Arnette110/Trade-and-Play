7397af0802945e979b175675d0164427e128ca81
Tucker Vassau <tuckv@hotmail.com>
2020-04-26 19:11:33 -0600

    Scroller example in place w/ error in console

--- client/src/components/Collection/index.js
+++ client/src/components/Collection/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Grid } from '@material-ui/core';
-import CardFlip from '../../pages/CardFlip';
+import { Container } from '@material-ui/core';
+// import CardFlip from '../../pages/CardFlip';
+import ItemsCarousel from 'react-items-carousel';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,31 +24,37 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
+
 export default function NestedGrid() {
   const classes = useStyles();
 
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid className={classes.root} item xs={3}>
-        <CardFlip/>
-        </Grid>
-        <Grid className={classes.root} item xs={3}>
-        <CardFlip/>
-        </Grid>
-        <Grid  className={classes.root}item xs={3}>
-        <CardFlip/>
-        </Grid>
-        <Grid  className={classes.root}item xs={3}>
-        <CardFlip/>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+
+  const [activeItemIndex, setActiveItemIndex] = React.useState(0);
+  const chevronWidth = 40;
+
+  // function FormRow() {
+  //   return (
+  //     <React.Fragment>
+  //       <Grid className={classes.root} item xs={3}>
+  //       <CardFlip/>
+  //       </Grid>
+  //       <Grid className={classes.root} item xs={3}>
+  //       <CardFlip/>
+  //       </Grid>
+  //       <Grid  className={classes.root}item xs={3}>
+  //       <CardFlip/>
+  //       </Grid>
+  //       <Grid  className={classes.root}item xs={3}>
+  //       <CardFlip/>
+  //       </Grid>
+  //     </React.Fragment>
+  //   );
+  // }
 
   return (
     <div className={classes.root}>
-        <Container>
+      {/* <Container>
       <Grid container spacing={1}>
         <Grid container item xs={12} spacing={0}>
           <FormRow />
@@ -62,7 +69,36 @@ export default function NestedGrid() {
           <FormRow />
         </Grid>
       </Grid>
+      </Container> */}
+
+      {/* Scroller code snippet */}
+      <Container>
+        <div style={{ padding: `20 ${chevronWidth}px` }}>
+          <ItemsCarousel
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={3}
+            gutter={20}
+            leftChevron={<button>{'<'}</button>}
+            rightChevron={<button>{'>'}</button>}
+            outsideChevron
+            chevronWidth={chevronWidth}
+          >
+            <div style={{ height: 300, background: '#EEE' }}>First Card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Second card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Third Card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Fourth card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Fifth card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Sixth card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Seventh card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Eighth card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Ninth card</div>
+            <div style={{ height: 300, background: '#EEE' }}>Tenth card</div>
+          </ItemsCarousel>
+        </div>
       </Container>
+      {/* Scroller end code */}
+
     </div>
   );
 }
