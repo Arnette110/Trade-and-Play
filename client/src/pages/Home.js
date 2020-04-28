e0841571b2ac7d8050ba3ae21fd0ff963853adab
Tyler Snyder <tylersnyder619406@gmail.com>
2020-04-28 15:11:36 -0600

    console log return object of user

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import { makeStyles } from '@material-ui/core/styles'
 import { Grid, Typography, Container } from "@material-ui/core"
+import auth0Client from "../Auth";
+
+
 
 
 // const useStyles = makeStyles((theme) => ({
@@ -17,6 +20,15 @@ import { Grid, Typography, Container } from "@material-ui/core"
 
 export default function Home() {
 
+  // const { loading, user } = useAuth0();
+
+  const user = auth0Client.getProfile()
+
+    if (!user) {
+      return <div>Loading...</div>;
+    }
+
+
     // const classes = useStyles()
    
 
@@ -24,16 +36,18 @@ export default function Home() {
       <Grid container>
         <Grid item xs={12}>
           <Container>
-            <Typography style={{ overflowWrap: 'inherit' }}>
+            <Typography style={{ overflowWrap: "inherit" }}>
+              {console.log(user)}
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
               porro hic. Quos modi libero voluptatem odit consequatur aut fugiat
               temporibus pariatur sit, accusantium voluptates ducimus aliquid!
               Rerum dignissimos repellat architecto!
+              <img src={user.picture} alt={user.picture}/>
             </Typography>
           </Container>
         </Grid>
       </Grid>
-    )
+    );
 }
 
 
