4a199d7634b78eee01821e3cab10d4bb418f07f6
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-27 18:48:03 -0600

    set more styling

--- client/src/components/Booster/index.js
+++ client/src/components/Booster/index.js
@@ -1,47 +1,63 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardActionArea, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  root: {
-    marginTop: '20px',
-    borderRadius: 'unset'
-  },
-  media: {
-    height: '100%',
-    width: '100%',
-  },
+    root: {
+        marginTop: '20px',
+        borderRadius: 'unset'
+    },
+    media: {
+        height: '100%',
+        width: '100%',
+    },
 });
 
 function Booster(props) {
-  const classes = useStyles();
-  const { title, description, img } = props
+    const classes = useStyles();
+    const { title, description, img } = props
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea disableRipple disabled>
-        <img className={classes.media} alt='Booster Packs' src={img}/>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p" style={{height: '100px'}}>
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button size="medium" variant="contained" color="primary">
-          Buy Booster Pack
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card className={classes.root}>
+            <CardActionArea disabled>
+                <img className={classes.media} alt='Booster Packs' src={img} />
+                <CardContent>
+                    <Typography
+                        gutterBottom
+                        variant="h5"
+                        // component="h2"
+                    >
+                        {title}
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        style={{ height: '100px' }}
+                    >
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions
+                style={{ justifyContent: 'space-evenly' }}
+            >
+                <Button
+                    size="medium"
+                    variant="contained"
+                    color="primary"
+                >
+                    Draft Now
+                </Button>
+                <Button
+                    size="medium"
+                    variant="contained"
+                    color="primary"
+                >
+                    See Roster
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
 
 export default Booster
--- client/src/pages/Packs.js
+++ client/src/pages/Packs.js
@@ -7,24 +7,26 @@ function Packs() {
 
     return (
         <Grid container
+            alignItems="stretch"
+            justify='space-evenly'
             direction='row'
-            justify='center'
-            spacing={isMedium ? 0 : 5}>
-            <Grid item xs={12} sm={8} md={4}>
+            // spacing={isMedium ? 0 : 5}
+        >
+            <Grid item xs={12} sm={8} md={3}>
                 <Booster
                     title='Forwards'
                     description="Increase the offensive power of your collection with the top NHL's forwards. The season's top scorers avaialable in a single pack "
                     img='https://nhl.bamcontent.com/images/photos/316717774/2048x1152/cut.jpg'
                 />
             </Grid>
-            <Grid item xs={12} sm={8} md={4}>
+            <Grid item xs={12} sm={8} md={3}>
                 <Booster
                     title='Defensemen'
                     description="Your collection can become a defensive powerhouse with the top blue lines. The season's best defensemen avaialable in a single pack"
                     img='https://nhl.bamcontent.com/images/photos/316632660/2048x1152/cut.jpg'
                 />
             </Grid>
-            <Grid item xs={12} sm={8} md={4}>
+            <Grid item xs={12} sm={8} md={3}>
                 <Booster
                     title='Goalie'
                     description="Make your collection untouchable with the best Goalies in the league. The season's best goalies all available in a single pack."
