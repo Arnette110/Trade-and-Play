96ecf7d744a52da439af1a45a429ff39c2e2cb3d
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-29 13:39:06 -0600

    redesign of landing-page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,51 +1,62 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Typography, Container } from "@material-ui/core"
-import auth0Client from "../Auth";
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Typography, Container } from '@material-ui/core'
+import auth0Client from '../Auth'
+import logo from '../Logo.svg'
+import Jumbotron from '../components/Jumbotron'
+import Login from '../components/login'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
 
-
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//     justifyContent: 'center',
-//     textAlign: "center",
-//     overflow: 'hidden',
-//     backgroundColor: theme.palette.background.paper,
-//   },
-  
-// }))
+const useStyles = makeStyles({
+  textContainer: {
+    background: 'rgb(235,36,39)',
+    border: 0,
+    borderRadius: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    marginTop: '5rem',
+  },
+})
 
 export default function Home() {
-
-
-
-    // const classes = useStyles()
-     const user = auth0Client.getProfile();
-
-     if (!user) {
-       return <div>Loading...</div>;
-     }
-
-
-    return (
-      <Grid container>
-        <Grid item xs={12}>
-          <Container>
-            {console.log(user)}
-            <img src={user.picture} alt={user.picture} />
-            <Typography style={{ overflowWrap: "inherit" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-              porro hic. Quos modi libero voluptatem odit consequatur aut fugiat
-              temporibus pariatur sit, accusantium voluptates ducimus aliquid!
-              Rerum dignissimos repellat architecto!
-            </Typography>
-          </Container>
+  return (
+    <div>
+      <Jumbotron>
+        <Grid container>
+          <Grid item xs={12}>
+            <Container>
+              <img src={logo} alt='my logo'></img>
+            </Container>
+          </Grid>
+          <br />
+          <Grid item xs={12}>
+            <Container>
+              <Typography
+                variant='h4'
+                style={{ paddingTop: '2rem', fontFamily: 'Roboto Black' }}>
+                YOU'RE THE BOSS!
+              </Typography>
+              <br />
+              <Container style={{display: 'flex', justifyContent: 'center'}}>
+                <StarBorderIcon fontSize='large'/>{' '}
+                <Typography display='inline' variant='h5' style={{}}>
+                  Get booster packs to build your team deck
+                </Typography>
+              </Container>
+              <Container>
+                <StarBorderIcon fontSize='large' />{' '}
+                <Typography display='inline' variant='body1'>
+                  Get booster packs to build your team deck
+                </Typography>
+              </Container>
+            </Container>
+          </Grid>
+          <Grid item xs={12}>
+            <Login />
+          </Grid>
         </Grid>
-      </Grid>
-    );
+      </Jumbotron>
+    </div>
+  )
 }
-
-
-
