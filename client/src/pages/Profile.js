14fb4203074c6d50529e499b405f70684d87dba2
Carlos Hernandez <carlos@devcodify.com>
2020-04-29 01:19:49 -0600

    Create picture and button profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -52,30 +52,33 @@
 //     )
 // }
 
-// export default Profile;
-
-
+// export default Profile
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import FilledInput from '@material-ui/core/FilledInput';
+import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
-
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-
 import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
-            margin: theme.spacing(1),
+            margin: theme.spacing(2),
             width: '25ch',
         },
+    },
+    large: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+
     },
 }));
 
@@ -89,6 +92,7 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function SimpleSelect() {
+
     const classes = useStyles();
     const [Gender, setGender] = React.useState('');
 
@@ -96,13 +100,17 @@ export default function SimpleSelect() {
         setGender(event.target.value);
     };
 
+
+
     return (
         <div>
 
             {/* picture */}
             <div>
 
-
+                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                <Avatar alt="Remy Sharp" src="https://avatars1.githubusercontent.com/u/56489980?s=460&u=474c8bfae2757c5101e04e4a5e41e0b078c3d226&v=4" className={classes.large} />
+                <Button variant="contained" > Choose File </Button>
 
             </div>
 
@@ -110,37 +118,54 @@ export default function SimpleSelect() {
             {/* form */}
             <div>
 
-
-                <form className={classes.root} noValidate autoComplete="off">
-
-                    <TextField id="outlined-basic" label="prueba" variant="outlined" />
-
-                    <TextField id="outlined-textarea" label="Multiline Placeholder" placeholder="Placeholder" multiline variant="outlined" />
-
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-outlined-label"
-                            id="demo-simple-select-outlined"
-                            value={Gender}
-                            onChange={handleChange}
-                            label="Gender"
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={"Male"}>Ten</MenuItem>
-                            <MenuItem value={"Female"}>Twenty</MenuItem>
-
-                        </Select>
-                    </FormControl>
+                <Grid container justify="space-around">
 
 
 
 
 
-                </form>
+
+
+
+
+
+
+                    <form className={classes.root} noValidate autoComplete="off">
+
+                        <TextField id="outlined-basic" label="First Name" variant="outlined" />
+                        <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                        <TextField id="outlined-basic" label="Phone" variant="outlined" />
+                        <TextField id="outlined-basic" label="Email" variant="outlined" />
+
+                        <TextField id="outlined-textarea" label="Multiline Placeholder" placeholder="Placeholder" multiline variant="outlined" />
+
+
+
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-outlined-label"
+                                id="demo-simple-select-outlined"
+                                value={Gender}
+                                onChange={handleChange}
+                                label="Gender">
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value={"Male"}>Ten</MenuItem>
+                                <MenuItem value={"Female"}>Twenty</MenuItem>
+
+                            </Select>
+                        </FormControl>
+
+
+
+
+
+                    </form>
+                </Grid>
             </div>
         </div>
     );
 }
+
