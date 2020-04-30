7b874428fc61688560c67d4e20c7e6f5c0443429
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-29 21:55:42 -0600

    start of tabs

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import logo from '../logo.svg'
-import Logout from '../components/Logout'
 import NavBar from '../components/NavBar'
+import DashboardTabs from '../components/Dashboard'
 
 export default function Dashboard() {
     return (
         <div>
             <NavBar/>
+            <DashboardTabs/>
         </div>
     )
 }
