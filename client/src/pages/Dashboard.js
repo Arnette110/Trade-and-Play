6c8d2100e6ede04fc2e6abc6b5fc4e487a001ed2
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-01 21:31:00 -0600

    removed unused imports

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-// import logo from '../logo.svg'
-// import Logout from '../components/Logout'
 import NavBar from '../components/NavBar'
 import DashboardTabs from '../components/Dashboard'
 
