b5c234fb09bbdc68c7c83fdf4f09ff74129aa900
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-01 19:40:55 -0600

    commented line 14

--- client/src/components/RosterListItem/index.js
+++ client/src/components/RosterListItem/index.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RosterLi(props) {
     const classes = useStyles();
-    build the rest of this key.
+    // build the rest of this key.
     const key = [{
         fullName: 'Boston Bruins',
         abrName: 'BOS'
@@ -26,6 +26,7 @@ export default function RosterLi(props) {
         if (el.fullName === props.data.currentTeam.name) {
             props.data.currentTeam.abrName = el.abrName
         }
+        
     })}
     getAbrName(key)
     
