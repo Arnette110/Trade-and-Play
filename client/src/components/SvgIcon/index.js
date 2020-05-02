7d6d1f6ebca51eadc4c315722a86209b349c05ae
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-01 21:14:35 -0600

    removed unused imports and commented code

--- client/src/components/SvgIcon/index.js
+++ client/src/components/SvgIcon/index.js
@@ -1,13 +1,5 @@
 import React from 'react'
 import logo from '../../logo.svg'
-import SvgIcon from '@material-ui/core/SvgIcon'
-import { makeStyles } from '@material-ui/core/styles'
-
-// const useStyles = makeStyles({
-//     logo: {
-//         fon
-//     }
-// })
 
 export default function LogoIcon() {
     return (
