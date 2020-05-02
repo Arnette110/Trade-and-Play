70f5a393431e2d27acc9f0210002c93cfd9163f0
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-01 21:13:59 -0600

    added styling for background of dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,23 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 // import logo from '../logo.svg'
 // import Logout from '../components/Logout'
 import NavBar from '../components/NavBar'
 import DashboardTabs from '../components/Dashboard'
 
+const useStyles = makeStyles({
+  background: {
+    backgroundColor: '#424242',
+    height: '100vh',
+    width: '100vw'
+  },
+})
+
 export default function Dashboard() {
+    const classes = useStyles()
+
     return (
-        <div>
+        <div className={classes.background}>
             <NavBar/>
             <DashboardTabs/>
         </div>
