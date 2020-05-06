a8ff4a771eb4083e65b7bc0c0bbf62f21f44fe16
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-05 19:54:01 -0600

    set app js to mirror master

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 // React
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // Components
 // import ButtonAppBar from './components/NavBar'
 // import Collection from './components/Collection'
@@ -15,7 +12,7 @@ import Home from './pages/Home'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
+// import Profile from './pages/Profile'
 import Draft from './pages/Draft'
 
 // import CardFlip from './pages/CardFlip'
@@ -23,10 +20,12 @@ import PlayerCreate from './pages/PlayerCreate'
 // Css
 import './App.css'
 import PrivateRoute from './hocs/PrivateRoute'
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles'
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui/core/styles'
 // import UnPrivateRoute from './hocs/UnPrivateRoute';
-
-
 
 const theme = createMuiTheme({
   palette: {
@@ -43,78 +42,32 @@ const theme = createMuiTheme({
 })
 
 function App() {
-  
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Switch>
           <Route exact path={['/', '/home']} component={Home} color='primary' />
-          <PrivateRoute exact path='/createplayer' roles={['admin']}>
-            <PlayerCreate />
-          </PrivateRoute>
-          <PrivateRoute path='/dashboard' roles={['user', 'admin']}>
-            <Dashboard />
-          </PrivateRoute>
+          <PrivateRoute
+            exact
+            path='/createplayer'
+            roles={['admin']}
+            component={PlayerCreate}></PrivateRoute>
+          <PrivateRoute
+            path='/dashboard'
+            roles={['user', 'admin']}
+            component={Dashboard}></PrivateRoute>
+          <PrivateRoute
+            path={['/draft']}
+            roles={['user', 'admin']}
+            component={Draft}
+          />
           <Route exact path='/register' component={SignupPage} />
           <Route exact path='/login' component={LoginPage} />
           <Route exact path='/nomatch' component={NoMatch} />
-          <Route exact path='/profile' component={Profile} />
         </Switch>
       </Router>
     </ThemeProvider>
   )
-    <Router>
-      <div>
-        <Route exact path={["/", "/home"]} component={Home} />
-        <PrivateRoute
-          path="/createplayer"
-          roles={["admin"]}
-          component={PlayerCreate}
-        />
-        <PrivateRoute
-          path={["/dashboard"]}
-          roles={["user", "admin"]}
-          component={Dashboard}
-        />
-        <PrivateRoute
-          path={['/draft']}
-          roles={['user', 'admin']}
-          component={Draft}
-        />
-        <Route exact path="/register" component={SignupPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/nomatch" component={NoMatch} />
-      </div>
-    </Router>
-
-    // <Router>
-    //   <div>
-    //     <Switch>
-    //       <Route exact path={["/", "/home"]} component={Home} />
-    //       <PrivateRoute path="/createplayer" roles={["admin"]}>
-    //         <PlayerCreate />
-    //       </PrivateRoute>
-    //       <PrivateRoute
-    //         exact
-    //         path="/collection"
-    //         roles={["user", "admin"]}
-    //         component={Collection}
-    //       />
-    //       <PrivateRoute
-    //         exact
-    //         path="/dashboard"
-    //         roles={["user", "admin"]}
-    //         component={Dashboard}
-    //       />
-    //       {/* <Dashboard />
-    //       </PrivateRoute> */}
-    //       <Route exact path="/register" component={SignupPage} />
-    //       <Route exact path="/login" component={LoginPage} />
-    //       <Route exact path="/nomatch" component={NoMatch} />
-    //     </Switch>
-    //   </div>
-    // </Router>
-  );
 }
 
 export default App
