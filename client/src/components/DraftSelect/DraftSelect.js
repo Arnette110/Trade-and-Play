93b896d0e44e9b1b7cfb1a14ba5fd73575a4b342
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-06 22:12:13 -0600

    more fix

--- client/src/components/DraftSelect/DraftSelect.js
+++ client/src/components/DraftSelect/DraftSelect.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import {Link} from 'react-router-dom'
 import { Button } from '@material-ui/core'
 import CardFlip from '../../pages/CardFlip'
 import API from '../../utils/API'
@@ -14,18 +15,25 @@ function DraftSelect({ data }) {
     const {user} = useContext(AuthContext);
 
     return (
-        <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', alignItems: 'center', marginTop: '20px'}}>
-            <CardFlip data={data} style={{marginBottom: '20px'}}/>
-            <Button
-                onClick={() => addToCollection(user._id, data._id)}
-                href={'/dashboard'}
-                variant='contained'
-                color='primary'
-                style={{marginTop: '20px'}}
-            >
-                Draft {data.bio.fullName}
-            </Button>
-        </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignContent: 'center',
+          alignItems: 'center',
+          marginTop: '20px',
+        }}>
+        <CardFlip data={data} style={{ marginBottom: '20px' }} />
+        <Link to='/dashboard'>
+          <Button
+            onClick={() => addToCollection(user._id, data._id)}
+            variant='contained'
+            color='primary'
+            style={{ marginTop: '20px' }}>
+            Draft {data.bio.fullName}
+          </Button>
+        </Link>
+      </div>
     )
 }
 export default DraftSelect
